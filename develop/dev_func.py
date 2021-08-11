4b94e5a4d3ba947fafded50b2c8bf97f28748d7c
Kelvinthedrugger <890225kelvin@gmail.com>
2021-08-11 23:41:09 +0800

    works for layer with shape (5,)

--- develop/dev_func.py
+++ develop/dev_func.py
@@ -1,27 +1,14 @@
 """use to test functions that are more complicated"""
 import numpy as np
 
-"""
-add dropout
-
-deactivate units in the specified layer with probability = prob
-
-for ith unit in layer1:
- # requires storage:(i, ith unit)
- if ith unit = 0: # previously dropped out
-  ith unit = storage(i, ith unit)
- index = uniform(0,1)
- if index < prob:
-  unit = 0
-"""
-
 
 def dropout(layer, nth_layer=0, prob=0.2, storage=None):
-    # try to prevent stroage got cleaned if unwanted
+    """# try to prevent stroage got cleaned if unwanted"""
     # idk how to append value w/ dict
     if storage is None:  # first epoch in training
         storage = {}  # {ith, value}
     # only one layer is assigned
+    """maybe one layer is fine"""
     if isinstance(nth_layer, int):
         for i in range(len(layer)):
             if layer[i].all() == 0.:
@@ -31,21 +18,29 @@ def dropout(layer, nth_layer=0, prob=0.2, storage=None):
                 # if not layer[i].all() == 0.:
                 storage[i] = layer[i]
                 print("\nindex: ", i, " should store: ", layer[i])
-                layer[i] *= 0.
+                #layer[i] *= 0.
+                if storage[i].all() != 0.:
+                    layer[i] *= 0.
     return layer, storage
 
 
 testlayer = np.random.uniform(-10., 10., size=(5, 5))
-
-for i in range(5):  # training loop
+"""
+for i in range(1):  # training loop
     print("\nepoch: %d" % (i+1))
     print("\nbefore: \n\n", testlayer)
     after_dropout, storage = dropout(
         testlayer, storage=(None if i == 0 else storage))
     print("\nafter: \n\n", after_dropout)
-    #print("\nstorage: \n\n", storage)
-    # if not storage == {}:
-    #     for j in range(5):  # it's gonna be slow asf
-    #         if testlayer[j][0] == 0.:
-    #             print(storage[j])  # indexing is problematic
-print(storage)
+
+print("\n\n", storage)"""
+store = {}
+testlayer = np.random.uniform(-10., 10., size=(5, ))
+if np.random.uniform() < 0.8:
+    print(testlayer)
+    store[0] = testlayer[0]
+    print("\nindex: ", 0, " should store: ", testlayer[0])
+    testlayer[0] *= 0.
+    print(testlayer)
+
+print(store)

2daee9770b9e26afd55d86181ad7cbe032233d22
Kelvinthedrugger <890225kelvin@gmail.com>
2021-08-11 23:51:52 +0800

    run by reference mode so noe of the assigns work if we change the layer value regardless of the running order

--- develop/dev_func.py
+++ develop/dev_func.py
@@ -24,8 +24,8 @@ def dropout(layer, nth_layer=0, prob=0.2, storage=None):
     return layer, storage
 
 
-testlayer = np.random.uniform(-10., 10., size=(5, 5))
 """
+testlayer = np.random.uniform(-10., 10., size=(5, 5)).astype(np.float32)
 for i in range(1):  # training loop
     print("\nepoch: %d" % (i+1))
     print("\nbefore: \n\n", testlayer)
@@ -34,13 +34,24 @@ for i in range(1):  # training loop
     print("\nafter: \n\n", after_dropout)
 
 print("\n\n", storage)"""
+
+
+def pas_val(x):
+    return x
+
+
 store = {}
-testlayer = np.random.uniform(-10., 10., size=(5, ))
+testlayer = np.random.uniform(-10., 10., size=(5, 5)).astype(np.float32)
 if np.random.uniform() < 0.8:
     print(testlayer)
-    store[0] = testlayer[0]
+    tmp = testlayer[0]  # assign here but didnt work
+    print(tmp)
+    tmp1 = pas_val(testlayer[0])
+    print(tmp1)
+    #store[0] = tmp
     print("\nindex: ", 0, " should store: ", testlayer[0])
     testlayer[0] *= 0.
-    print(testlayer)
-
+    print(testlayer, type(testlayer[0, 0]))
+print(tmp)
+print(tmp1)
 print(store)

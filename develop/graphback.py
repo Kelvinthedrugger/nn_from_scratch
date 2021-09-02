e6e368a002daa1a06462d7047a989b3000474483
Kelvinthedrugger <890225kelvin@gmail.com>
2021-09-02 11:59:45 +0800

    cleanup, not done

--- develop/graphback.py
+++ develop/graphback.py
@@ -100,14 +100,19 @@ class Learner:
     # like tensorflow api
 
     def __init__(self, model, lossf, optimizer):
-        self.model = model  # figure out how to assemble the layers
-        self.lossf = lossf  # loss function instead of the class
-        self.optimizer = optimizer  # function also
+        """i don't care of using list but i guess i have to """
+        # figure out how to assemble the layers
+        # list.extend method could be useful
+        self.model = model
+        # loss function instead of the class
+        self.lossf = lossf
+        # function also
+        self.optimizer = optimizer
 
     def __call__(self, x, y):
         """forward-backward"""
         yhat = self.model(x)
-        loss, gradient = self.lossf(yhat, y)
+        self.lossf(yhat, y)
         self.optimizer()
 
 
